ad23e99bf8f7e25926ad588fa0cb1592a915231e
DaveM <dave.miranda@gmail.com>
2020-04-16 20:42:42 -0400

    part 1

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const { getAllTeams, getTeamById, saveNewTeam } = require('./controllers/teams')
+
+const app = express()
+
+app.get('/', getAllTeams)
+
+app.get('/:id', getTeamById)
+
+app.post('/teams', bodyParser.json(), saveNewTeam)
+
+
+
+app.listen(1337, () => {
+  console.log('listening on port 1337...') // eslint-disable-line no-console
+})
