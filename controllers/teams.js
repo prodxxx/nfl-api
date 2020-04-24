682805269e8280a789fc3a50b5f0f5e7b9f09102
DaveM <dave.miranda@gmail.com>
2020-04-23 21:17:21 -0400

    fixed saeve new team route

--- controllers/teams.js
+++ controllers/teams.js
@@ -16,14 +16,14 @@ const getTeamById = async (request, response) => {
   return response.send(foundTeam)
 }
 
-const saveNewTeam = (request, response) => {
+const saveNewTeam = async (request, response) => {
   const { location, mascot, abbreviation, conference, division } = request.body
 
   if (!location || !mascot || !abbreviation || !conference || !division) {
     return response.status(400).send('Required fields are: location, mascot, abbreviation, conference, division')
   }
 
-  const newTeam = { location, mascot, abbreviation, conference, division }
+  const newTeam = await models.teams.create({ location, mascot, abbreviation, conference, division })
 
   teams.push(newTeam)
 
